9921137bb97e2f5ba8387fea86a4d8b219786b29
Bagitbek <bagitbek1@gmail.com>
2022-07-13 11:52:45 +0600

    Add @NotNull annotation to fields of PostModel

--- src/main/java/com/example/postcoreapi/model/PostModel.java
+++ src/main/java/com/example/postcoreapi/model/PostModel.java
@@ -11,13 +11,13 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 
 public class PostModel {
-//    @NotNull(message = "Post id cannot be null")
+    @NotNull(message = "Post id cannot be null")
     private String postId;
-//    @NotNull(message = "Client id cannot be null")
+    @NotNull(message = "Client id cannot be null")
     private String clientId;
     @Size(min=5, max=20,message = "Name of client must be greater than 2 and less than 16")
     private String nameOfClient;
-//    @NotNull(message = "postRecipientId cannot be null")
+    @NotNull(message = "postRecipientId cannot be null")
     private String postRecipientId;
     @Size(min=5, max=20,message = "Name of recipient must be greater than 2 and less than 16")
     private String nameOfRecipient;
